e1077d0bbca802ba763bdcb0b7ce4a113d076d63
Kim Bauters <kim.bauters@smarthound.uk>
2021-07-23 10:56:11 +0100

    add ZIMClient documentation and better error handling

--- zimply/zim_core.py
+++ zimply/zim_core.py
@@ -100,6 +100,25 @@ def to_bytes(data, encoding):
 
 
 #####
+# Common error classes
+#####
+class ZIMException(Exception):
+    pass
+
+
+class ZIMFileUnpackError(Exception):
+    pass
+
+
+class ZIMClientNoFile(ZIMException):
+    pass
+
+
+class ZIMClientInvalidFile(ZIMException):
+    pass
+
+
+#####
 # Definition of a number of basic structures/functions to simplify the code
 #####
 
@@ -479,16 +498,20 @@ class ZIMFile:
         # open the file as a binary file
         self.file = open(filename, "rb")
         # retrieve the header fields
-        self.header_fields = HeaderBlock(self._enc).unpack_from_file(self.file)
-        self.major = int(self.header_fields["major_version"])
-        self.minor = int(self.header_fields["minor_version"])
-        self.version = (self.major, self.minor)
-        self.mimetype_list = MimeTypeListBlock(self._enc).unpack_from_file(self.file, self.header_fields["mimeListPos"])
-        # create the object once for easy access
-        self.redirectEntryBlock = RedirectEntryBlock(self._enc)
+        try:
+            self.header_fields = HeaderBlock(self._enc).unpack_from_file(self.file)
+            self.major = int(self.header_fields["major_version"])
+            self.minor = int(self.header_fields["minor_version"])
+            self.version = (self.major, self.minor)
+            self.mimetype_list = MimeTypeListBlock(self._enc).unpack_from_file(self.file,
+                                                                               self.header_fields["mimeListPos"])
+            # create the object once for easy access
+            self.redirectEntryBlock = RedirectEntryBlock(self._enc)
 
-        self.articleEntryBlock = ArticleEntryBlock(self._enc)
-        self.clusterFormat = ClusterBlock(self._enc)
+            self.articleEntryBlock = ArticleEntryBlock(self._enc)
+            self.clusterFormat = ClusterBlock(self._enc)
+        except struct_error:
+            raise ZIMFileUnpackError
 
     def checksum(self, extra_fields=None):
         # create a checksum to uniquely identify this zim file
@@ -1080,18 +1103,39 @@ class XapianIndex(SearchIndex):
 
 
 class ZIMClient:
-    def __init__(self, zim_filename, encoding, index_file=None, auto_delete=False):
-        # create the object to access the ZIM file
-        # TODO: ensure file exists before continuing
-        # TODO: verify that file is a valid ZIM file? throw ZIM error?
-        self._zim_file = ZIMFile(zim_filename, encoding)
+    def __init__(self, zim_filename, encoding="utf-8", index_file=None, auto_delete=False):
+        """ Create a new ZIM client to easily access the provided ZIM file.
+        :param zim_filename: the path to the file to open as a ZIM file.
+        :param encoding: the encoding used in the ZIM file which is usually UTF-8 - this is not verified for you!
+        :param index_file: the location of where to create an index file if relying on SQLite FTS search.
+                           At this location a file *.idx and a file *.chk will be created.
+        :param auto_delete: by default the ZIMClient (silently) fails when a SQLite FTS index is opened
+                            for which the checksum (of the ongoing indexation) fails. By enabling this
+                            option the incorrect index will be deleted and recreated instead.
+        :raises:
+            ZIMClientNoFile: if zim_filename is recognised as a path to a file.
+            ZIMClientInvalidFile: if the file at zim_filename could not be successfully opened as a ZIM file.
+        """
+
+        if not os.path.isfile(zim_filename):
+            raise ZIMClientNoFile
+
+        self._zim_file = None
         self.encoding = encoding
 
-        # determine the language
+        try:
+            # create the object to access the ZIM file
+            self._zim_file = ZIMFile(zim_filename, encoding)
+        except ZIMFileUnpackError:
+            raise ZIMClientInvalidFile
+
+        # determine the language if set in the ZIM file
         default_iso = to_bytes("eng", encoding=encoding)
         iso639 = self._zim_file.metadata().get("language", default_iso).decode(encoding=encoding, errors="ignore")
         self.language = iso639_3to1.get(iso639, "en")
-        logging.info("ZIM file: language: " + self.language + " (ISO639-1), articles: " + str(len(self._zim_file)))
+        version = str(self._zim_file.major) + "," + str(self._zim_file.minor)
+        logging.info("ZIM file + (" + str(version) + ") - language: " +
+                     self.language + " (ISO639-1), articles: " + str(len(self._zim_file)))
 
         if not index_file:
             base = os.path.basename(zim_filename)
